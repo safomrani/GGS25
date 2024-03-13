539f3b5fbbe0d02e3eff6add6b011b42f7c2c6d9
HodaAhmad <hudaanis28@gmail.com>
2024-03-13 17:49:59 +0200

    adding buttons

--- green-growth/app/components/hero.tsx
+++ green-growth/app/components/hero.tsx
@@ -40,14 +40,14 @@ export default function Hero() {
             backgroundPosition: 'right top',
             backgroundSize: 'contain'
         }}>
-            <div className="pt-10 px-3 md:px-24 w-[90%] md:w-3/4">
-                <h1 className="text-center md:text-left text-3xl sm:text-3xl md:text-5xl lg:text-5xl text-green-300"><span className={`${karlaExtraBold.className}`}>Green Growth Summit 2024</span></h1>
-                <div className=" justify-center md:w-3/5 md:pb-5">
-                    <h1 className={`${karla.className}pt-3 text-1xl text-black text-justify`}>Under the regional Green Growth Summit initiative, Hivos convenes two flagship summit events to invigorate local green ecosystems to North Africa to advance their green transition strategies, facilitate meaningful partnerships between stakeholders, and to synergize around actionable commitments to take the green ecosystem forward.</h1>
+            <div className="pt-10 px-3 md:px-12 lg:px-24  md:w-[680px] lg:w-[865px]">
+                <h1 className="text-center md:text-left text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-green-300"><span className={`${karlaExtraBold.className}`}>Green Growth Summit 2024</span></h1>
+                <div className=" justify-center md:pb-5 md:w-[86%] lg:w-full">
+                    <h1 className={`${karla.className}pt-3 md:text-[18px] lg:text-1xl text-black text-justify`}>Join us to accelerate a just green transition! Hivos is convening two flagship Green Growth Summits to invigorate local green ecosystems in the Middle East and North Africa. Interested in forming meaningful partnerships? Have ideas for how to take green transition strategies to the next level? Register your interest in attending at the links below.</h1>
                 </div>
             </div>
             {/**Event details */}
-            <div className="pl-16 md:pl-24 w-3/4 flex flex-row gap-48 pb-10">
+            <div className="pl-16 md:pl-12 lg:pl-24 w-3/4 flex flex-row md:gap-20 lg:gap-48 pb-10">
                 <div className="flex flex-col gap-3">
                     <div className="pb-2">
                         <button className={`${karlaBold.className} text-[16px] text-white bg-green-300 px-1 rounded-md`}>
@@ -73,13 +73,11 @@ export default function Hero() {
                         Mövenpick Gammarth - Tunis
                     </div>
                     <div className="pt-2 flex flex-col">
-                        {/*
                         <button className= {`${karla.className} bg-red-300 uppercase 
                         text-white px-3 py-2 rounded-md w-[180px] text-[18px]`}>
-                            <a href='https://form.typeform.com/to/OgeyBBu3' target="_blank">Register Now*</a>
+                            <a href='https://docs.google.com/forms/d/e/1FAIpQLSdbn80m2mOZYvEIRqnbSzSKIf3nBeJGiSVOndUSYSfQuE70Ng/viewform' target="_blank">Register Now*</a>
                         </button>
                         <div className={`${karla.className} px-3 text-[13px]`}>*invitations are limited</div>
-                        */}
                     </div>
                 </div>
                 <div className="flex flex-col gap-3">
@@ -107,13 +105,11 @@ export default function Hero() {
                         Conrad Hotel - Cairo
                     </div>
                     <div className="pt-2 flex flex-col">
-                        {/*
                         <button className= {`${karla.className} bg-red-300 uppercase 
                         text-white px-3 py-2 rounded-md w-[180px] text-[18px]`}>
-                            <a href='https://form.typeform.com/to/wHZL1GST' target="_blank">Register Now*</a>
+                            <a href='https://docs.google.com/forms/d/e/1FAIpQLScv86Nlo6bfTL4Z0YozgeJWjgQ2tiK-mF9XppWS2a1qv4TB6Q/viewform' target="_blank">Register Now*</a>
                         </button>
                         <div className={`${karla.className} px-3 text-[13px]`}>*invitations are limited</div>
-                        */}
                     </div>
                 </div>
             </div>
@@ -121,11 +117,17 @@ export default function Hero() {
         </div>
         {/**Mobile view */}
         <div>
-        <div className="flex md:hidden flex-col bg-green-200">
+        <div className="flex md:hidden flex-col bg-green-200"  style={{
+            backgroundImage:
+            "url('/images/Hero-mobile.png')",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right bottom',
+            backgroundSize: 'fit'
+        }}>
             <div className="flex flex-col justify-center items-center pt-10 text-justify">
                 <div className=" justify-center px-3 sm:w-[90%]">
                     <h1 className="py-1 text-center text-[22px] sm:text-2xl text-green-300"><span className={`${karlaExtraBold.className}`}>Green Growth Summit 2024</span></h1>
-                    <h1 className={`${karla.className} px-3 pt-3 text-[14px] text-black text-justify`}>Under the regional Green Growth Summit initiative, Hivos convenes two flagship summit events to invigorate local green ecosystems to North Africa to advance their green transition strategies, facilitate meaningful partnerships between stakeholders, and to synergize around actionable commitments to take the green ecosystem forward.</h1>
+                    <h1 className={`${karla.className} px-3 pt-3 text-[14px] text-black text-justify`}>Join us to accelerate a just green transition! Hivos is convening two flagship Green Growth Summits to invigorate local green ecosystems in the Middle East and North Africa. Interested in forming meaningful partnerships? Have ideas for how to take green transition strategies to the next level? Register your interest in attending at the links below.</h1>
                 </div>
             </div>
             {/**Event details */}
@@ -156,16 +158,14 @@ export default function Hero() {
                         Mövenpick Gammarth - Tunis
                     </div>
                     <div className="pt-2 flex flex-col">
-                        {/*
                         <button className= {`${karla.className} bg-red-300 uppercase 
                             text-white px-3 py-2 rounded-md w-[180px] text-[18px]`}>
-                            <a href='https://form.typeform.com/to/OgeyBBu3' target="_blank">Register *</a>
+                            <a href='https://docs.google.com/forms/d/e/1FAIpQLSdbn80m2mOZYvEIRqnbSzSKIf3nBeJGiSVOndUSYSfQuE70Ng/viewform' target="_blank">Register Now*</a>
                         </button>
                         <div className={`${karla.className} px-3 text-[13px]`}>*invitations are limited</div>
-                        */}
                     </div>
                 </div>
-                <Separator className='bg-red-300 w-3/4'/>
+                <Separator className='bg-red-300 w-2/3'/>
 
                 {/*Egypt**/}
                 <div className="flex flex-col gap-3">
@@ -193,29 +193,37 @@ export default function Hero() {
                         Conrad Hotel - Cairo
                     </div>
                     <div className="pt-2 flex flex-col">
-                        {/*
                         <button className= {`${karla.className} bg-red-300 uppercase 
                             text-white px-3 py-2 rounded-md w-[180px] text-[18px]`}>
-                            <a href='https://form.typeform.com/to/wHZL1GST' target="_blank">Register Now*</a>
+                            <a href='https://docs.google.com/forms/d/e/1FAIpQLScv86Nlo6bfTL4Z0YozgeJWjgQ2tiK-mF9XppWS2a1qv4TB6Q/viewform' target="_blank">Register Now*</a>
                         </button>
                         <div className={`${karla.className} px-3 text-[13px]`}>*invitations are limited</div>
-                        */}
                     </div>
                 </div>
             </div>
         </div>
         </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between md:pt-20 lg:pt-10 py-5">
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between lg:pt-6 py-5">
         {/**Sponsor & implementation partners */}
-        <div className="flex flex-wrap lg:flex-row gap-5 lg:gap-10 items-center justify-center md:pl-20"> 
+        <div className="flex flex-wrap lg:flex-row gap-5 lg:gap-10 items-center justify-center lg:pl-24"> 
           <div className="">
             <p className={`${karla.className} text-black pb-3 text-bold text-center lg:text-left`}>Main Sponsor</p>
             <div className="flex gap-7 py-3 lg:py-0 justify-center lg:justify-start">
               <img 
                 src='/logos/Hivos.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="Hivos logo"
+              />
+            </div>
+          </div>
+          <div className="">
+            <p className={`${karla.className} text-black pb-3 text-bold text-center lg:text-left`}>Core Partner</p>
+            <div className="flex gap-7 py-3 lg:py-0 justify-center lg:justify-start">
+              <img 
+                src='/logos/FMO.png'
+                className="h-[30px] lg:h-[40px]"
+                alt="FMO logo"
               />
             </div>
           </div>
@@ -224,12 +232,12 @@ export default function Hero() {
             <div className="flex gap-7 py-3 lg:py-0">
               <img 
                 src='/logos/newsilk-logo.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="newsilk logo"
               />
               <img 
                 src='/logos/changelabs.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="Changelabs logo"
               />
             </div>
@@ -237,28 +245,28 @@ export default function Hero() {
         </div>
 
         {/**Support from*/}
-        <div className="py-5 lg:py-0 md:pr-20">
+        <div className="py-5 lg:py-0 lg:pr-20">
           <div className="flex flex-col justify-center items-center lg:items-start">
-            <p className={`${karla.className} text-black pb-3 text-bold text-center lg:text-left`}>With Support from</p>
+            <p className={`${karla.className} text-black pb-3 text-bold text-center lg:text-left`}>With Support From</p>
             <div className="flex flex-wrap lg:flex justify-center items-center gap-7 py-3 lg:py-0">
               <img 
                 src='/logos/funded by EU.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="Funded by EU logo"
               />
               <img 
                 src='/logos/Impact Europe.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="Impact EU logo"
               />
               <img 
                 src='/logos/CFFYE.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="Challengefund logo"
               />
               <img 
                 src='/logos/MFAN.png'
-                className="h-[40px] lg:h-[60px]"
+                className="h-[30px] lg:h-[40px]"
                 alt="MFAN logo"
               />
             </div>
