5d2aaf027410af1569d671f1b082c0848d64548e
HodaAhmad <hudaanis28@gmail.com>
2024-04-19 12:31:01 +0200

    editing speakers and agenda

--- green-growth/app/data/agenda-tunis.js
+++ green-growth/app/data/agenda-tunis.js
@@ -4,24 +4,28 @@ export const agendaTunis = [
         events: [    
             {
                 from: '8:15',
-                title: 'Registration & Breakfast',
+                title: 'Registration & Welcome Coffee',
+                panelcolor: ''
             },
             {
                 from: '9:00',
                 title: 'The Green Growth Summit: Cultivating Strategic Alliances to Drive a Just Green Transition',
+                panelcolor: ''
             },
             {
                 from: '9:45',
                 title: 'The Future is Green: Building a Just & Green Transition in Tunisia',
-                
+                panelcolor: '#d73732'
             },
             {
                 from: '10:30',
                 title: 'Ecosystems Transformed: Unlocking the Green Economy`s Power to Create the Jobs of the Future',
+                panelcolor: '#01a98f'
             },
             {
                 from: '11:15',
                 title: 'Networking & Coffee Break',
+                panelcolor: '',
                 info: 
                 [
                     {   
@@ -32,35 +36,51 @@ export const agendaTunis = [
             {
                 from: '11:45',
                 title: 'The Time Is Now: Capitalizing on Green Investment Opportunities',
-
+                panelcolor: 'black'
             },
             {
                 from: '12:30',
-                title: 'Financing Green: Innovative Funding Mechanisms for Tunisia`s Green Transition'
+                title: 'Financing Green: Innovative Funding Mechanisms for Tunisia`s Green Transition',
+                panelcolor: '#e0593b'
             },
             {
                 from: '13:15',
-                title: 'Lunch'
+                title: 'Lunch',
+                panelcolor: ''
             },
             {
                 from: '14:15',
                 title: 'Afternoon Breakout Sessions',
+                panelcolor: '',
                 event: 
                 [
                     {   
-                        details: 'Education and Green Economy : Accelerating Skills Development for Green Jobs'
+                        details: 'Education and Green Economy : Accelerating Skills Development for Green Jobs',
+                        color: '#01a98f'
                     },
                     {
-                        details: 'Shared Economy for Green Jobs and Urban Resilience'
+                        details: 'Shared Economy for Green Jobs and Urban Resilience',
+                        color: '#d73732'
                     },
                     {
-                        details: 'Green Impact Investing in Tunisia'
+                        details: 'Green Impact Investing in Tunisia',
+                        color: 'black'
+                    },
+                    {
+                        details: 'Mobilizing Finance for Green Investments',
+                        color: '#e0593b'
                     }
                 ]
             },
             {
+                from: '16:00',
+                title: 'Donor Task Force for a Green Transition [Invitation Only]',
+                panelcolor: ''
+            },
+            {
                 from: '17:00',
-                title: 'Networking Reception'
+                title: 'Networking Reception',
+                panelcolor: ''
             }
         ]
     },
