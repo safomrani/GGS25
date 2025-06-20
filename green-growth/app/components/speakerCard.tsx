8e9cd628aa1e57cb526f1c6e32189a4bfb32b44f
Ali Mohamed Ali <3ligraphy@gmail.com>
2025-06-20 16:39:01 +0300

    Update SpeakerCard component and add new speaker details
    - Modified the SpeakerCard component to enhance layout and styling, including adjustments to minimum height and text properties for better readability.
    - Added a new speaker, Assistant Minister Mohamed Meatemed Eissawy, to the speakersEgypt data with associated image and LinkedIn link.
    - Introduced a new image file for the added speaker to improve visual representation.

--- green-growth/app/components/speakerCard.tsx
+++ green-growth/app/components/speakerCard.tsx
@@ -20,7 +20,7 @@ const SpeakerCard = ({
   return (
     <div className = 'relative z-10 flex flex-col items-center justify-end w-[90%] sm:w-full'>
       <div className="h-10 w-3/4 blur-md absolute shadow-lg shadow-[rgba(0,0,0,0.3)]" />
-      <div className='z-10 rounded-[24px] bg-[#F4F4F4] w-full sm:h-[370px] md:h-[355px] lg:h-[345px]'>
+      <div className='z-10 rounded-[24px] bg-[#F4F4F4] w-full min-h-[370px] sm:min-h-[380px] md:min-h-[375px] lg:min-h-[365px]'>
         <div className='w-full rounded-t-[24px]'>
           <img
             src={image_file}
@@ -30,10 +30,10 @@ const SpeakerCard = ({
         <div className='p-5'>
           <div className="flex items-start justify-between mb-1">
             <div className="flex flex-col gap-1 lg:w-[87%]">
-              <div className= {`${karlaExtraBold.className} text-xl lg:text-[20px] text-green-300 font-bold`}>{name}</div>  
-              <div className= {`${karla.className} text-[13px] text-[#454545]`}>{company}</div>
+              <div className= {`${karlaExtraBold.className} text-xl lg:text-[20px] text-green-300 font-bold leading-tight`}>{name}</div>  
+              <div className= {`${karla.className} text-[13px] text-[#454545] leading-normal break-words`}>{company}</div>
             </div>
-            <div>
+            <div className="flex-shrink-0">
             <Link target="_blank" rel="noopener noreferrer" href={`${link}`}>
               <img
                 src='/icons/Linkedin.png'
